975aa9cfc0db4aa13b89ad3ba484c50bbb2f2299
Jonathan Chernoch <33642531+chernobylx@users.noreply.github.com>
2026-06-02 10:39:11 -0400

    IDK

--- scrape.py
+++ scrape.py
@@ -1,5 +1,5 @@
 import pybaseball as pb
 def scrape_statcast(year, output_dir: str = 'data/statcast'):
-    df = pb.statcast(str(year)+"-03-01", str(year)+"-11-30")
+    df = pb.statcast(str(year)+"-02-01", str(year)+"-11-30")
     df.to_csv(output_dir+'/statcast_'+str(year)+'.csv', index=False)
 
