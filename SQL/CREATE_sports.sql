71afdce2df41633f21cb5342caf318bd075764df
Jonathan Chernoch <33642531+chernobylx@users.noreply.github.com>
2026-06-02 11:04:35 -0400

    named link constraint

--- SQL/CREATE_sports.sql
+++ SQL/CREATE_sports.sql
@@ -5,8 +5,13 @@ CREATE TABLE sports(
     sport_id INT PRIMARY KEY,-- MLB = 1
     sport_name VARCHAR(255) UNIQUE NOT NULL,-- e.g. Major League Baseball
     sport_code VARCHAR(4) UNIQUE NOT NULL,
-    sort_order INT CHECK ( sort_order > 0 ),
-    sport_link varchar(255) NOT NULL CHECK (sport_link ~ '^/api/v\d(?:\.\d+)?(?:/[a-zA-Z0-9]+)+$')
+    sort_order INT UNIQUE NOT NULL CHECK ( sort_order > 0 ),
+    sport_link varchar(255) UNIQUE NOT NULL,
+    ---------------------------------------------------------------------------------------
+    CONSTRAINT sport_link_regex CHECK (sport_link ~ '^/api/v\d(?:\.\d+)?(?:/[a-zA-Z0-9]+)+$')
 );
 
- COMMIT;
+COMMIT;
+--ROLLBACK TO SAVEPOINT create_sports;
+--DROP TABLE IF EXISTS sports;
+--COMMIT;
