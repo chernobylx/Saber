bf870a87e7eb44f9efbf0e6d78294357906cc222
Jonathan Chernoch <33642531+chernobylx@users.noreply.github.com>
2026-06-02 13:45:52 -0400

    import requests, define url def get_leagues()

--- extract.py
+++ extract.py
@@ -1,11 +1,24 @@
-from statsapi import get
+from statsapi import get as api
 import polars as pl
+from requests import get
 
+url = 'https://statsapi.mlb.com/api/v1/'
 def get_sports()-> pl.DataFrame:
     """Retrieve a DataFrame of available sports from the statsapi.mlb.com"""
     try:
-        sports = get('sports')['sports']
+        sports = api('sports')['sports']
         sports = pl.json_normalize(sports)
         return sports
     except Exception as e:
         raise
+
+
+def get_leagues()-> pl.DataFrame:
+    """Retrieve a DataFrame of available leagues from statsapi.mlb.com"""
+    try:
+        leagues = get(url+'leagues').json()['leagues']
+        leagues = pl.json_normalize(leagues)
+        return leagues
+    except Exception as e:
+        raise
+
