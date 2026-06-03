2be2caf3a1bc1a6ea0f87e9d780415ab12aa2c39
Jonathan Chernoch <33642531+chernobylx@users.noreply.github.com>
2026-06-03 09:38:47 -0400

    rename requests.get -> GET import Callable
    def get
    remove as e from except
    
    created endpoints dict

--- extract.py
+++ extract.py
@@ -1,32 +1,42 @@
 from statsapi import get as api
 import polars as pl
-from requests import get
-
+from requests import get as GET
+from typing import Callable
 url = 'https://statsapi.mlb.com/api/v1/'
+
+def get(endpoint:str)-> pl.DataFrame:
+
+    return endpoints[endpoint]()
 def get_sports()-> pl.DataFrame:
     """Retrieve a DataFrame of available sports from the statsapi.mlb.com"""
     try:
         sports = api('sports')['sports']
         sports = pl.json_normalize(sports)
         return sports
-    except Exception as e:
+    except Exception:
         raise
 
 
 def get_leagues()-> pl.DataFrame:
     """Retrieve a DataFrame of available leagues from statsapi.mlb.com"""
     try:
-        leagues = get(url+'leagues').json()['leagues']
+        leagues = GET(url+'leagues').json()['leagues']
         leagues = pl.json_normalize(leagues)
         return leagues
-    except Exception as e:
+    except Exception:
         raise
 
 def get_divisions()-> pl.DataFrame:
     """Retrieve a DataFrame of available divisions from statsapi.mlb.com"""
     try:
-        divisions = get(url+'/divisions').json()['divisions']
+        divisions = GET(url+'/divisions').json()['divisions']
         divisions = pl.json_normalize(divisions)
         return divisions
-    except Exception as e:
+    except Exception:
         raise
+
+endpoints:dict[str, Callable[[],pl.DataFrame]] = {
+    'sports': get_sports,
+    'leagues': get_leagues,
+    'divisions': get_divisions
+}
