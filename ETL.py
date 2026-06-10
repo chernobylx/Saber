b76b2ebc2443972dfdd29d3209e786f98578ae00
Jonathan Chernoch <33642531+chernobylx@users.noreply.github.com>
2026-06-10 12:54:08 -0400

    refactor etl.py to avoid name overloading

--- ETL.py
+++ ETL.py
@@ -1,7 +1,7 @@
 import marimo
 
 __generated_with = "0.23.9"
-app = marimo.App()
+app = marimo.App(width="full")
 
 
 @app.cell
@@ -19,7 +19,6 @@ def _():
 
     return (
         DivisionSchema,
-        DivisionSeasonsSchema,
         LeagueCollection,
         LeagueSchema,
         Path,
@@ -30,7 +29,6 @@ def _():
         TeamsBySeasonSchema,
         endpoints,
         get,
-        mo,
         pl,
         transform_divisions,
         transform_leagues,
@@ -43,6 +41,8 @@ def _():
 @app.cell
 def _(Path, endpoints):
     data = Path('data/statsapi')
+    if not data.exists():
+        data.mkdir(parents=True)
     tables = endpoints.keys()
     paths = [data/(table+'.parquet') for table in tables]
     return data, paths, tables
@@ -57,40 +57,55 @@ def _(get, paths, tables):
 
 
 @app.cell
-def _(data, pl):
+def _(
+    DivisionSchema,
+    LeagueCollection,
+    LeagueSchema,
+    SeasonSchema,
+    SportSchema,
+    SportsSeasons,
+    TeamSchema,
+    TeamsBySeasonSchema,
+    data,
+    pl,
+    transform_divisions,
+    transform_leagues,
+    transform_seasons,
+    transform_sports,
+    transform_teams,
+):
+    #scan disk
     sports = pl.scan_parquet(data/'sports.parquet')
     leagues = pl.scan_parquet(data/'leagues.parquet')
     divisions = pl.scan_parquet(data/'divisions.parquet')
     seasons = pl.scan_parquet(data/'seasons.parquet')
     teams = pl.scan_parquet(data/'teams.parquet')
-    return divisions, leagues, seasons, sports, teams
+    #apply schemas
+    sports = transform_sports(sports)
+    leagues = transform_leagues(leagues)
+    divisions, division_seasons = transform_divisions(divisions, leagues)
+    seasons = transform_seasons(seasons)
+    teams, team_seasons = transform_teams(teams)
+    #validate schemas
+    sports = SportSchema.validate(sports, cast=True).lazy()
+    leagues =  LeagueSchema.validate(leagues, cast=True).lazy()
+    seasons = SeasonSchema.validate(seasons, cast=True).lazy()
+    divisions = DivisionSchema.validate(divisions, cast=True).lazy()
+    teams = TeamSchema.validate(teams, cast=True).lazy()
+    team_seasons = team_seasons.with_columns(
+        pl.col.league_id.fill_null(-1)
+    )
+    team_seasons = TeamsBySeasonSchema.validate(team_seasons, cast=True).lazy()
 
-
-@app.cell
-def _(SportSchema, sports, transform_sports):
-    sports_1 = transform_sports(sports)
-    sports_1 = SportSchema.validate(sports_1, cast=True).lazy()
-    return (sports_1,)
-
-
-@app.cell
-def _(LeagueSchema, leagues, transform_leagues):
-    leagues_1 = transform_leagues(leagues)
-    leagues_1 = LeagueSchema.validate(leagues_1, cast=True).lazy()
-    return (leagues_1,)
-
-
-@app.cell
-def _(SeasonSchema, seasons, transform_seasons):
-    seasons_1 = transform_seasons(seasons)
-    seasons_1 = SeasonSchema.validate(seasons_1, cast=True).lazy()
-    return (seasons_1,)
-
-
-@app.cell
-def _(SportsSeasons, seasons_1, sports_1):
-    SC, bad = SportsSeasons.filter({'sports': sports_1, 'seasons': seasons_1})
-    return SC, bad
+    SC, bad = SportsSeasons.filter({'sports':sports, 'seasons':seasons})
+    LC = LeagueCollection.validate({
+        'leagues': leagues,
+        'divisions': divisions,
+        'seasons': SC.seasons,
+        'teams': teams,
+        'team_seasons': team_seasons
+    })
+    return bad, division_seasons
 
 
 @app.cell
@@ -100,55 +115,8 @@ def _(bad):
 
 
 @app.cell
-def _(
-    DivisionSchema,
-    DivisionSeasonsSchema,
-    divisions,
-    leagues_1,
-    transform_divisions,
-):
-    div, div_seasons = transform_divisions(divisions, leagues_1)
-    divisions_1 = DivisionSchema.validate(div, cast=True).lazy()
-    division_seasons = DivisionSeasonsSchema.validate(div_seasons, cast=True).lazy()
-    return (divisions_1,)
-
-
-@app.cell
-def _():
-    return
-
-
-@app.cell
-def _(TeamSchema, TeamsBySeasonSchema, pl, teams, transform_teams):
-    teams_1, team_seasons = transform_teams(teams)
-    teams_1 = TeamSchema.validate(teams_1, cast=True).lazy()
-    team_seasons = team_seasons.with_columns(
-        pl.col.league_id.fill_null(-1)
-    )
-    team_seasons = TeamsBySeasonSchema.validate(team_seasons, cast=True).lazy()
-    return (team_seasons,)
-
-
-@app.cell
-def _(team_seasons):
-    team_seasons.collect()
-    return
-
-
-@app.cell
-def _(mo, team_seasons):
-    mo.ui.dataframe(team_seasons.collect().to_pandas(), page_size=20)
-    return
-
-
-@app.cell
-def _(LeagueCollection, SC, divisions_1, leagues_1, team_seasons):
-    LC = LeagueCollection.validate({'leagues': leagues_1, 'divisions': divisions_1, 'seasons': SC.seasons, 'team_seasons': team_seasons})
-    return
-
-
-@app.cell
-def _():
+def _(division_seasons):
+    division_seasons.collect()
     return
 
 
